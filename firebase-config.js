4022931b2411d3298e890546047cf9989beffb4a
treza05github <nurohmantreza0005@gmail.com>
2025-11-05 15:22:42 +0700

    Refine comments in firebase-config.js
    Updated comments for clarity and removed unnecessary notes.

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-// firebase-config.js
 // Konfigurasi dan inisialisasi Firebase (modular SDK)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 
-// Ganti nilai di bawah dengan konfigurasi dari Firebase Console (jika ingin rahasiakan, biarkan seperti ini untuk testing)
+// konfigurasi dari Firebase Console
 const firebaseConfig = {
   apiKey: "AIzaSyCBgAE5vbWfan3ixH0D5V3Dwc0yCKeadyY",
   authDomain: "app-input-nilai-kel1.firebaseapp.com",
